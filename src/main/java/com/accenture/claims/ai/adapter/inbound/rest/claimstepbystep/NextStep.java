a1c2ba627738c88023d0de183efbfdd46a7a047e
gabriele.fuso.nerini <gabriele.fuso.nerini@accenture.com>
2025-09-12 18:27:43 +0200

    StepToolMessage - primo

--- src/main/java/com/accenture/claims/ai/adapter/inbound/rest/claimstepbystep/NextStep.java
+++ src/main/java/com/accenture/claims/ai/adapter/inbound/rest/claimstepbystep/NextStep.java
@@ -3,5 +3,5 @@ package com.accenture.claims.ai.adapter.inbound.rest.claimstepbystep;
 import com.arjuna.ats.jta.exceptions.NotImplementedException;
 
 public interface NextStep {
-    ClaimSubmissionStep nextStep(ClaimSubmissionProgress progress) throws NotImplementedException;
+    ClaimSubmissionStep nextStep(ClaimSubmissionProgress progress);
 }
--- src/main/java/com/accenture/claims/ai/adapter/inbound/rest/claimstepbystep/NextStepAdapter.java
+++ src/main/java/com/accenture/claims/ai/adapter/inbound/rest/claimstepbystep/NextStepAdapter.java
@@ -3,12 +3,12 @@ package com.accenture.claims.ai.adapter.inbound.rest.claimstepbystep;
 import com.arjuna.ats.jta.exceptions.NotImplementedException;
 
 public class NextStepAdapter implements NextStep{
-    public ClaimSubmissionStep nextStep(ClaimSubmissionProgress progress) throws NotImplementedException {
+    public ClaimSubmissionStep nextStep(ClaimSubmissionProgress progress) {
         for(ClaimSubmissionStep step : ClaimSubmissionStep.values()){
             if(!step.isComplete(progress)){
                 return step;
             }
         }
-        throw new NotImplementedException("No more steps available");
+        throw new RuntimeException("No more steps available");
     }
 }
